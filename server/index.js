867115fa8595b75fa6cb95fd3b589e2f71eb0827
Rohan Das P <rohan2003710@gmail.com>
2025-05-20 20:49:26 +0530

    dashboard

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.get('/auth/github/callback', passport.authenticate('github', {
   session: true
 }), (req, res) => {
   console.log('User authenticated:', req.user);
-  res.redirect('https://github-oath-frontend.onrender.com');
+  res.redirect('https://github-oath-frontend.onrender.com/dashboard');
 });
 
 app.get('/api/repo', ensureAuthenticated, async (req, res) => {
